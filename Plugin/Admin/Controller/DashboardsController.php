9868013125b239066fd05b32c7f33f62493a2940
ca.huynh <cahs@evolable.asia>
2014-05-23 18:35:12 +0700

    init respository for tisemizapp developing on CakePHP

---
+++ Plugin/Admin/Controller/DashboardsController.php
@@ -0,0 +1,11 @@
+<?php
+App::uses('AdminAppController', 'Admin.Controller');
+/**
+ * DoanhNghieps Controller
+ *
+ */
+class DashboardsController extends AdminAppController {
+
+	public $layout = 'admin/default';
+	public function index(){}
+}
---
+++ Plugin/Admin/Controller/DoanhNghiepsController.php
@@ -0,0 +1,17 @@
+<?php
+App::uses('AdminAppController', 'Admin.Controller');
+/**
+ * DoanhNghieps Controller
+ *
+ */
+class DoanhNghiepsController extends AdminAppController {
+
+/**
+ * Scaffold
+ *
+ * @var mixed
+ */
+//	public $scaffold;
+	public $layout = 'admin/default';
+	public function lietke(){}
+}
